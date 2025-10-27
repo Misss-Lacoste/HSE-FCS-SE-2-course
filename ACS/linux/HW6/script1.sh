0321b1ab0b1d1339858c55297e557b83f8d0f77c
Miss Lacoste <makarrroninka@yandex.ru>
2025-10-27 23:40:44 +0300

    added new bash scripts

---
+++ ACS/linux/HW6/script1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This is a comment
+echo "The current directory is: "
+pwd 
+echo "The user logged in as: "
+whoami
+echo "I have 1$ in my pocket!"
+if pwd
+then
+echo "It works!"
+fi
---
+++ ACS/linux/HW6/script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "hello!"
+if echo "I am "
+whoami
+then
+echo "my current working directory is: "
+pwd
+echo "and as I am logged as Margaret, "
+echo "obviously I am programmer and I have 1$ in my pocket! Hurray!"
+fi
